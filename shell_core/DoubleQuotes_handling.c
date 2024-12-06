5764d5ee2ed3c49f9bba83a5f0bee82f13116488
Fabio Andre Silva Pinto Bastos <fsilva-p@c1r7s3.42luxembourg.lu>
2024-12-06 19:19:20 +0100

    started double quotes handling

---
+++ shell_core/DoubleQuotes_handling.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DoubleQuotes_handling.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsilva-p <fsilva-p@42luxembourg.lu>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/06 19:14:28 by fsilva-p          #+#    #+#             */
+/*   Updated: 2024/12/06 19:18:15 by fsilva-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+void double_quotes(char *symbol)
+{
+	
+}
+
+int main()
+{
+	char test1[] = "Hello";
+	double_quotes(test1);
+	printf("\n");
+}
